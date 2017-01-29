3a3e5a5d8802f20379fcc5475c114945c506e261
rivlev <proflevitan@gmail.com>
2017-01-29 16:03:44 -0500

    first commit

---
+++ bawe_gender_classifier.py
@@ -0,0 +1,50 @@
+# detect author gender from essay corpus
+
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+import feature_extractor as fe
+import numpy
+import re
+import sys
+
+datadir = sys.argv[1]
+genderlabfile = datadir + "/BAWE_balanced_subset.csv"
+conffile = sys.argv[2]
+
+def load_balanced_gender_labels():
+	'''
+	Read the gender labels file and return dictionary mapping student id
+	to gender
+	'''
+	meta_lines = [line.rstrip().split(',') for line in open(genderlabfile)]
+	gender_dict = {row[0]:row[1] for row in meta_lines[1:]}
+	return gender_dict
+
+def load_essays(gender_dict):
+	essays = []
+	genderlabels = []
+	students = []
+	for student, gender in gender_dict.items():
+		with open('%s/%s.txt' % (datadir, student)) as f:
+			text = f.read()
+			text = re.sub('<[^<]+?>', '', text)		# remove vestigial xml
+			essays.append(text)
+			genderlabels.append(gender)
+			students.append(student)
+	return essays, genderlabels, students
+
+def load_conf_file():
+	conf = set(line.strip() for line in open(conffile))
+	return conf
+
+def predict_gender(X, Y):
+	scores = cross_val_score(GaussianNB(), X, Y, scoring='accuracy', cv=10)
+	return scores.mean()
+
+if __name__ == "__main__":
+	gender_dict = load_balanced_gender_labels()
+	essays, genderlabels, students = load_essays(gender_dict)
+	conf = load_conf_file()
+	features = fe.extract_features(essays, conf)
+
+	print (predict_gender(features, genderlabels))
---
+++ feature_extractor.py
@@ -0,0 +1,67 @@
+# extract features from list of text instances based on configuration set of features
+
+import nltk
+import numpy
+import re
+
+source_text = []
+stemmed_text = []
+
+def preprocess():
+	# first stem and lowercase words, then remove rare
+	# lowercase 
+	global source_text
+	source_text = [text.lower() for text in source_text]
+
+	# tokenize
+	tokenized_text = [nltk.word_tokenize(text) for text in source_text]
+	
+	# stem
+	porter = nltk.PorterStemmer()
+	global stemmed_text
+#	stemmed_text = [[porter.stem(t) for t in tokens] for tokens in tokenized_text]
+	for tokens in tokenized_text: # iterating instead of list comprehension to allow exception handling
+		stemmed_line = []
+		for t in tokens:			
+			try:
+				stemmed_line.extend(porter.stem(t))
+			except IndexError:
+				stemmed_line.extend('')
+		stemmed_text.append(stemmed_line)
+
+	# remove rare
+#	vocab = nltk.FreqDist(w for w in line for line in stemmed_text)
+	vocab = nltk.FreqDist(w for line in stemmed_text for w in line)
+	rarewords_list = vocab.hapaxes()
+	rarewords_regex = re.compile(r'\b(%s)\b' % '|'.join(rarewords_list))
+	stemmed_text = [[rarewords_regex.sub('<RARE>', w) for w in line] for line in stemmed_text]
+	# note that source_text will be lowercased, but only stemmed_text will have rare words removed
+
+def bag_of_function_words():
+	bow = []
+	for sw in nltk.corpus.stopwords.words('english'):
+		counts = [sum(1 for _ in re.finditer(r'\b%s\b' % sw, line)) for line in source_text]
+		bow.append(counts)
+	return bow	
+
+# FILL IN OTHER FEATURE EXTRACTORS
+
+def extract_features(text, conf):
+	all = False
+	if len(conf)==0:
+		all = True
+
+	global source_text
+	source_text = text			# we'll use global variables to pass the data around
+	preprocess()
+
+	features = []		# features will be list of lists, each component list will have the same length as the list of input text
+
+
+	# extract requested features: FILL IN HERE
+	if 'bag_of_function_words' in conf or all:
+		features.extend(bag_of_function_words())
+
+	features = numpy.asarray(features).T.tolist() # transpose list of lists sow its dimensions are #instances x #features
+
+	return features
